43ac7382f724ef22dfe5251f46f9061d8e032515
Alexandre Henrique <alexandrehcdc@gmail.com>
2018-05-19 17:15:57 -0300

    master Add basic landing page view controller, pods and folder structure
    -
    
    Signed-off-by: Alexandre Henrique <alexandrehcdc@gmail.com>

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ajuda-me' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ajuda-me
+  pod 'Alamofire', '~> 4.0'
+  pod 'SwiftyJSON', '~> 4.0'
+  target 'ajuda-meTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ajuda-meUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- ajuda-me/ViewController.swift
+++ ajuda-me/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainBtn: UIButton!
+    
+    @IBAction func mainBtnAction(_ sender: Any) {
+        guard let number = URL(string: "tel://10101010") else { return }
+        UIApplication.shared.open(number)
+        print("aqui")
+    }
+    
     override func viewDidLoad() {
+        mainBtn.layer.cornerRadius = 20
+        mainBtn.clipsToBounds = true
+        
+        
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +32,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
