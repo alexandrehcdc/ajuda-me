9f1e803cba7711f3047db64205cc5bd2a6ee2249
Alexandre Henrique <alexandrehcdc@gmail.com>
2018-05-21 23:12:23 -0300

    master Add tableview to the structure
    -
    
    Signed-off-by: Alexandre Henrique <alexandrehcdc@gmail.com>

--- ajuda-me/ViewController.swift
+++ ajuda-me/ViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     @IBOutlet weak var mainBtn: UIButton!
     
     @IBAction func mainBtnAction(_ sender: Any) {
@@ -27,10 +27,27 @@ class ViewController: UIViewController {
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        print("chamou pso")
+        return "Tipos de emergência"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("chamou poa")
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlainCell", for: indexPath)
+        cell.textLabel?.text = "boa mlk"
+        print("chamou po")
+        return cell
+    }
+    
 }
 
