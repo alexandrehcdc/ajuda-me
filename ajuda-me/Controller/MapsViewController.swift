3e18c2e676f8ae83649abb81d2c08cf21a918f64
Alexandre Henrique <alexandrehcdc@gmail.com>
2018-07-19 13:47:22 -0300

    master Fix segue destinations and realm import
    -
    
    Signed-off-by: Alexandre Henrique <alexandrehcdc@gmail.com>

--- ajuda-me/Controller/MapsViewController.swift
+++ ajuda-me/Controller/MapsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 import GoogleMaps
 import GooglePlaces
 import SwiftyJSON
@@ -14,13 +15,18 @@ import SwiftyJSON
 class MapsViewController: UIViewController {
     
     let apiKey = "AIzaSyBQR4o1QgKELUJ-gpmpBfDONvfTQJ78jnY"
-    
     var mapPoints = [ServicePoint]()
-
+    var markers = [GMSMarker]()
+    let realm = try! Realm()
+    var servicePointLocalDataSource: ServicePointDataSource?
+    
     @IBOutlet weak var mapsView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        servicePointLocalDataSource = ServicePointDataSourceImpl.getInstance(realm: realm)
+        mapPoints = (servicePointLocalDataSource?.findAll())!
     }
     
     override func loadView() {
@@ -34,46 +40,53 @@ class MapsViewController: UIViewController {
         GMSPlacesClient.provideAPIKey(apiKey)
         
         //Coordenadas de alagoas
-        let camera = GMSCameraPosition.camera(withLatitude: -9.6498, longitude: -35.7089, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: -9.665203, longitude: -35.731192, zoom: 14.5)
         let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
         view = mapView
         
         // Creates a marker in the center of the map.
         
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -9.6498, longitude: -35.7089)
+        marker.position = CLLocationCoordinate2D(latitude: -9.665203, longitude: -35.731192)
         
         marker.title = "Alagoas"
         marker.snippet = "Brasil"
         marker.map = mapView
+        
+        mapPoints.forEach { point in
+            let newMarker = GMSMarker()
+            
+            newMarker.title = point.name
+            newMarker.snippet = point.city
+            newMarker.position = CLLocationCoordinate2DMake(Double(point.firstCoordinate)!, Double(point.secondCoordinate)!)
+            
+            newMarker.map = mapView
+        }
     
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
     }
     
     func markerFactory(response: JSON) -> Void {
-//        print(response[0]["geometry"].stringValue)
-        print(response)
-//            print(response[0].stringValue)
+
         for (index, obj) in response {
             
             let point = ServicePoint()
             point.name = obj["properties"]["Nome"].stringValue
             point.firstCoordinate = obj["geometry"]["coordinates"][0].stringValue
             point.secondCoordinate = obj["geometry"]["coordinates"][1].stringValue
-            point.address = obj["properties"]["Endereço"]
-            point.city = obj["properties"]["Município"]
+            point.address = obj["properties"]["Endereço"].stringValue
+            point.city = obj["properties"]["Município"].stringValue
+            servicePointLocalDataSource?.save(entity: point)
             mapPoints.append(point)
-            //            point.name = obj["geometry"]["coordinates"]
         }
         
         var marker = GMSMarker()
         marker.title = "Alagoas"
         marker.snippet = "Brasil"
-//        return marker
-//        marker.map = mapView
+        markers.append(marker)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 }
